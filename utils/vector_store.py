1da192c092504b70d27562703d670629ca5e0953
Utus Karta Sanggam <88187616+utusks001@users.noreply.github.com>
2025-08-26 14:14:25 +0700

    Create vector_store.py

---
+++ utils/vector_store.py
@@ -0,0 +1,9 @@
+from langchain.vectorstores import Chroma
+from langchain.embeddings import HuggingFaceEmbeddings
+from chromadb.config import Settings
+
+def create_vector_store(chunks):
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    chroma_settings = Settings(anonymized_telemetry=False, allow_reset=True, is_persistent=False)
+    vector_store = Chroma.from_texts(chunks, embedding=embeddings, client_settings=chroma_settings)
+    return vector_store
