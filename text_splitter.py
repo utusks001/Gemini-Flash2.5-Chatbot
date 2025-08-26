8a26f50fe5eb1461468029ecf3c8a3873ca9b2ab
Utus Karta Sanggam <88187616+utusks001@users.noreply.github.com>
2025-08-26 17:39:57 +0700

    Delete text_splitter.py

--- text_splitter.py
+++
@@ -1,8 +0,0 @@
-# text_splitter.py
-
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-
-def split_text(text, chunk_size=10000, chunk_overlap=1000):
-    splitter = RecursiveCharacterTextSplitter(
-        chunk_size=chunk_size, chunk_overlap=chunk_overlap)
-    return splitter.split_text(text)
