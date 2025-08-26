57599bb2161dbbc907b608eb4492dd01f854f829
Utus Karta Sanggam <88187616+utusks001@users.noreply.github.com>
2025-08-26 17:40:25 +0700

    Delete utils/pdf_handler.py

--- utils/pdf_handler.py
+++
@@ -1,9 +0,0 @@
-from PyPDF2 import PdfReader
-
-def extract_text_from_pdfs(pdf_docs):
-    text = ""
-    for pdf in pdf_docs:
-        reader = PdfReader(pdf)
-        for page in reader.pages:
-            text += page.extract_text()
-    return text
