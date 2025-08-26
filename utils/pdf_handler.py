4fc16db6be68b438a1f53d48a51c5a5193a56014
Utus Karta Sanggam <88187616+utusks001@users.noreply.github.com>
2025-08-26 14:12:48 +0700

    Create pdf_handler.py

---
+++ utils/pdf_handler.py
@@ -0,0 +1,9 @@
+from PyPDF2 import PdfReader
+
+def extract_text_from_pdfs(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        reader = PdfReader(pdf)
+        for page in reader.pages:
+            text += page.extract_text()
+    return text
