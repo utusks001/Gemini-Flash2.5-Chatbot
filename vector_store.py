1df206468b730615a9fe6d0bd9ab9d0a139c6ab2
Utus Karta Sanggam <88187616+utusks001@users.noreply.github.com>
2025-08-26 13:52:55 +0700

    Update vector_store.py

--- vector_store.py
+++ vector_store.py
@@ -5,11 +5,9 @@ from langchain.embeddings import HuggingFaceEmbeddings
 def create_vector_store(chunks):
     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
     vector_store = Chroma.from_texts(chunks, embedding=embeddings)
-    return vector_store  # return langsung tanpa persist
+    return vector_store
 
-def load_vector_store(chunks):
-    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-    return Chroma.from_texts(chunks, embedding=embeddings)
+
 
 
 
