006d79b95fd481fa71e69791232330d3c090cc9d
Utus Karta Sanggam <88187616+utusks001@users.noreply.github.com>
2025-08-28 12:28:05 +0700

    Update embedder.py

--- modules/embedder.py
+++ modules/embedder.py
@@ -1,7 +1,8 @@
 # Embedding & vector store
 from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import FAISS
+from langchain.vectorstores import Chroma
 
-def build_faiss_from_documents(docs):
+def build_chroma_from_documents(docs, persist_dir="chroma_store"):
     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-    return FAISS.from_documents(docs, embedding=embeddings)
+    vector_db = Chroma.from_documents(docs, embedding=embeddings, persist_directory=persist_dir)
+    return vector_db
