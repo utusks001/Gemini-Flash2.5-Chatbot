5a67b4197624b9bfa5cec5e90cfc4f86cef70324
Utus Karta Sanggam <88187616+utusks001@users.noreply.github.com>
2025-08-28 10:26:27 +0700

    Create embedder.py

---
+++ modules/embedder.py
@@ -0,0 +1,6 @@
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+
+def build_faiss_from_documents(docs):
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    return FAISS.from_documents(docs, embedding=embeddings)
