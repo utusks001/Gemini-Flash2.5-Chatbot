b1bfc2cfb892477406ed2286815546638d16fe25
Utus Karta Sanggam <88187616+utusks001@users.noreply.github.com>
2025-08-28 19:04:33 +0700

    Delete modules/chain.py

--- modules/chain.py
+++
@@ -1,6 +0,0 @@
-# Integrasi LLM + retriever
-# modules/chain.py
-from langchain.chains import RetrievalQA
-
-def build_qa_chain(llm, retriever):
-    return RetrievalQA.from_chain_type(llm=llm, retriever=retriever, return_source_documents=True)
