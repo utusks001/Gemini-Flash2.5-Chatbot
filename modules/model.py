90ce45383cee4880fc5af9fabf330ee42da5c482
Utus Karta Sanggam <88187616+utusks001@users.noreply.github.com>
2025-08-28 19:05:20 +0700

    Delete modules/model.py

--- modules/model.py
+++
@@ -1,6 +0,0 @@
-# modules/model.py
-import os
-from langchain_google_genai import ChatGoogleGenerativeAI
-
-def load_llm():
-    return ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0.2)
