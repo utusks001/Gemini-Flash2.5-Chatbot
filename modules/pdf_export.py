bea329b1ff19bbf1adb728030b13036c19b4fc37
Utus Karta Sanggam <88187616+utusks001@users.noreply.github.com>
2025-08-28 12:13:38 +0700

    Update pdf_export.py

--- modules/pdf_export.py
+++ modules/pdf_export.py
@@ -1,6 +1,5 @@
 # Export Jawaban ke PDF
 # modules/pdf_export.py
-
 from reportlab.lib.pagesizes import A4
 from reportlab.pdfgen import canvas
 from io import BytesIO
@@ -8,8 +7,7 @@ from io import BytesIO
 def export_answer_to_pdf(answer: str) -> BytesIO:
     buffer = BytesIO()
     c = canvas.Canvas(buffer, pagesize=A4)
-    width, height = A4
-    text_object = c.beginText(40, height - 50)
+    text_object = c.beginText(40, A4[1] - 50)
     text_object.setFont("Helvetica", 12)
     for line in answer.split("\n"):
         text_object.textLine(line)
