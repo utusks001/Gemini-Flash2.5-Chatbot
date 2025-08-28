6b919b1d1b74ddf042443c562e4fbbf4d123a38b
Utus Karta Sanggam <88187616+utusks001@users.noreply.github.com>
2025-08-28 12:10:27 +0700

    Update embedder.py

--- modules/embedder.py
+++ modules/embedder.py
@@ -1,8 +1,6 @@
 # Embedding & vector store
-# modules/embedder.py
-
-from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_community.vectorstores import FAISS
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
 
 def build_faiss_from_documents(docs):
     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
